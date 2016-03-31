729abaf661cd2b6342927acd1ba0caf68d098440
djordjenestorovic18214 <djordje_95@hotmail.com>
2016-03-31 02:05:15 +0200

    Napravljena JavaDoc dokumentacija za klasu Twitter.

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -2,16 +2,34 @@ package com.twitter;
 
 import java.util.LinkedList; 
 import com.twitter.poruke.TwitterPoruka;
-
+/**
+ * Ovo je klasa koja predstavlja Twitter.
+ * @author Djole
+ *
+ */
 public class Twitter { 
 	
+	/**
+	 * Lista objekata klase TwitterPoruka.
+	 */
 	private LinkedList<TwitterPoruka> poruke =
 			new LinkedList<TwitterPoruka>();
 	
+	/**
+	 * Metoda koja vraca vrednost atributa poruke.
+	 * @return lista poruka
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke(){
 		return poruke;
 		}
 	
+	/**
+	 * Metoda prima kao parametre atribute 'korisnik' i 'poruka' klase TwitterPoruka,
+	 * pa pravi jedan objekat te klase i dodaje mu prosledjene podatke.
+	 * Zatim taj objekat dodaje na kraj liste poruke.
+	 * @param korisnik Nova vrednost za atribut korisnik klase TwitterPoruka.
+	 * @param poruka Nova vrednost za atribut poruka klase TwitterPoruka.
+	 */
 	public void unesi(String korisnik, String poruka) {
 		//Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
@@ -21,6 +39,21 @@ public class Twitter {
 		poruke.addLast(tp);
 		}
 	
+	/**
+	 * Metoda koja kao parametre prima tag i maxBroj,
+	 * pa u listi poruke pretrazuje sve poruke i trazi one koje u sebi sadrze tag.
+	 * Te poruke zatim skladisti u niz rezultat koji je napravljen, sve dok se ne pretraze
+	 * sve poruke, ili sve dok se ne napuni niz. Kapacitet niza je prosledjen parametrom maxBroj.
+	 * Na kraju se taj niz vraca kao povratna vrednost metode.
+	 * @param maxBroj Kapacitet niza u koji se skladiste poruke koje sadrzie tag.
+	 * @param tag String koji se trazi u porukama.
+	 * @return niz sa porukama
+	 * @throws java.lang.RuntimeException ako je uneti tag:
+	 * <ul>
+	 * <li>null</li>
+	 * <li>prazan string</li>
+	 * </ul>
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag==null || tag == "")
 		throw new RuntimeException("Morate uneti tag");
